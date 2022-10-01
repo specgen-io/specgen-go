8f171e2ec4f9670744a73d6f498772c67fd9144f
specgen-automation <automation@specgen.io>
2022-10-01 22:42:35 +0000

    Release v2.1.2061-alpha

---
+++ v2/client/signature.go
@@ -0,0 +1,31 @@
+package client
+
+import (
+	"fmt"
+	"github.com/specgen-io/specgen-golang/v2/common"
+	"strings"
+
+	"github.com/specgen-io/specgen-golang/v2/goven/spec"
+	"github.com/specgen-io/specgen-golang/v2/responses"
+	"github.com/specgen-io/specgen-golang/v2/types"
+)
+
+func OperationSignature(operation *spec.NamedOperation, apiPackage *string) string {
+	return fmt.Sprintf(`%s(%s) %s`,
+		operation.Name.PascalCase(),
+		strings.Join(common.OperationParams(operation), ", "),
+		operationReturn(operation, apiPackage),
+	)
+}
+
+func operationReturn(operation *spec.NamedOperation, responsePackageName *string) string {
+	if common.ResponsesNumber(operation) == 1 {
+		response := operation.Responses[0]
+		return fmt.Sprintf(`(*%s, error)`, types.GoType(&response.Type.Definition))
+	}
+	responseType := responses.ResponseTypeName(operation)
+	if responsePackageName != nil {
+		responseType = *responsePackageName + "." + responseType
+	}
+	return fmt.Sprintf(`(*%s, error)`, responseType)
+}
--- v2/service/implementation.go
+++ v2/service/implementation.go
@@ -3,7 +3,6 @@ package service
 import (
 	"fmt"
 
-	"github.com/specgen-io/specgen-golang/v2/common"
 	"github.com/specgen-io/specgen-golang/v2/goven/generator"
 	"github.com/specgen-io/specgen-golang/v2/goven/spec"
 	"github.com/specgen-io/specgen-golang/v2/imports"
@@ -41,7 +40,7 @@ func generateServiceImplementation(api *spec.Api, apiModule, modelsModule, targe
 	w.EmptyLine()
 	apiPackage := api.Name.SnakeCase()
 	for _, operation := range api.Operations {
-		w.Line(`func (service *%s) %s {`, serviceTypeName(api), common.OperationSignature(&operation, &apiPackage))
+		w.Line(`func (service *%s) %s {`, serviceTypeName(api), OperationSignature(&operation, &apiPackage))
 		singleEmptyResponse := len(operation.Responses) == 1 && operation.Responses[0].Type.Definition.IsEmpty()
 		if singleEmptyResponse {
 			w.Line(`  return errors.New("implementation has not added yet")`)
@@ -60,36 +59,32 @@ func generateServiceImplementation(api *spec.Api, apiModule, modelsModule, targe
 func isContainsModel(api *spec.Api) bool {
 	for _, operation := range api.Operations {
 		if operation.Body != nil {
-			if isModel(&operation.Body.Type.Definition) {
+			if types.IsModel(&operation.Body.Type.Definition) {
 				return true
 			}
 		}
 		for _, param := range operation.QueryParams {
-			if isModel(&param.Type.Definition) {
+			if types.IsModel(&param.Type.Definition) {
 				return true
 			}
 		}
 		for _, param := range operation.HeaderParams {
-			if isModel(&param.Type.Definition) {
+			if types.IsModel(&param.Type.Definition) {
 				return true
 			}
 		}
 		for _, param := range operation.Endpoint.UrlParams {
-			if isModel(&param.Type.Definition) {
+			if types.IsModel(&param.Type.Definition) {
 				return true
 			}
 		}
 		for _, response := range operation.Responses {
-			if isModel(&response.Type.Definition) {
+			if types.IsModel(&response.Type.Definition) {
 				return true
 			}
 		}
 	}
 	return false
-}
-
-func isModel(def *spec.TypeDef) bool {
-	return def.Info.Model != nil
 }
 
 func serviceTypeName(api *spec.Api) string {
