f2b5fe05c87da56f5af076f1e78266db2f258a66
specgen-automation <automation@specgen.io>
2022-10-11 23:22:01 +0000

    Release v2.1.2119-alpha

--- v2/client/errors.go
+++ v2/client/errors.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-	"fmt"
 	"github.com/specgen-io/specgen-golang/v2/goven/generator"
 	"github.com/specgen-io/specgen-golang/v2/goven/spec"
 	"github.com/specgen-io/specgen-golang/v2/imports"
@@ -11,8 +10,7 @@ import (
 )
 
 func httpErrors(module, errorsModelsModule module.Module, errors *spec.Responses) *generator.CodeFile {
-	w := writer.NewGoWriter()
-	w.Line("package %s", module.Name)
+	w := writer.New(module, "errors.go")
 
 	imports := imports.New()
 	imports.Add("fmt")
@@ -26,10 +24,7 @@ func httpErrors(module, errorsModelsModule module.Module, errors *spec.Responses
 	internalServerError := errors.GetByStatusName(spec.HttpStatusInternalServerError)
 	getError(w, internalServerError)
 
-	return &generator.CodeFile{
-		Path:    module.GetPath(fmt.Sprintf("errors.go")),
-		Content: w.String(),
-	}
+	return w.ToCodeFile()
 }
 
 func getError(w generator.Writer, response *spec.Response) {
